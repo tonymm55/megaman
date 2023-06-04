0e55c16145539e1e8f5de331257559823a95681f
TonyMM55 <tonymmoran@gmail.com>
2023-06-04 11:40:56 +0100

    setup the folders according to tutorial

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from '../../assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import styles from './styles.module.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [count, setCount] = useState(10);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <div className={styles.main}></div>
+      <h1>Megaman vs Samus</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>{/* Edit <code>src/App.jsx</code> */}</p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {/* Click on the Vite and React logos to learn more */}
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+// export default App;
