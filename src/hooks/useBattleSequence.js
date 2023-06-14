ec119b459a4cc473fdc86b1b12c69ee518092065
TonyMM55 <tonymmoran@gmail.com>
2023-06-14 21:33:39 +0100

    completed battleSequence hook

--- src/hooks/useBattleSequence.js
+++ src/hooks/useBattleSequence.js
@@ -1,14 +1,24 @@
+import {
+  wait,
+  attack,
+  magic,
+  heal,
+  opponentStats,
+  playerStats,
+} from '../shared';
 import { useState, useEffect } from 'react';
-import { wait, attack, opponentStats, playerStats } from '../shared';
 
 export const useBattleSequence = sequence => {
   const [turn, setTurn] = useState(0);
   const [inSequence, setInSequence] = useState(false);
+
   const [playerHealth, setPlayerHealth] = useState(playerStats.maxHealth);
   const [opponentHealth, setOpponentHealth] = useState(
     opponentStats.maxHealth
   );
+
   const [announcerMessage, setAnnouncerMessage] = useState('');
+
   const [playerAnimation, setPlayerAnimation] = useState('static');
   const [opponentAnimation, setOpponentAnimation] = useState('static');
 
@@ -20,13 +30,13 @@ export const useBattleSequence = sequence => {
       const receiver = turn === 0 ? opponentStats : playerStats;
 
       switch (mode) {
-        case 'attack':
+        case 'attack': {
+          //block scoping 1:16:00
           const damage = attack({ attacker, receiver });
 
           (async () => {
             setInSequence(true);
             setAnnouncerMessage(`${attacker.name} has chosen to attack!`);
-
             await wait(1000);
 
             turn === 0
@@ -48,8 +58,6 @@ export const useBattleSequence = sequence => {
               ? setOpponentAnimation('static')
               : setPlayerAnimation('static');
             setAnnouncerMessage(`${receiver.name} felt that damage!`);
-            await wait(750);
-
             turn === 0
               ? setOpponentHealth(h => (h - damage > 0 ? h - damage : 0))
               : setPlayerHealth(h => (h - damage > 0 ? h - damage : 0));
@@ -63,9 +71,96 @@ export const useBattleSequence = sequence => {
           })();
 
           break;
+        }
 
-        default:
+        case 'magic': {
+          const damage = magic({ attacker, receiver });
+
+          (async () => {
+            setInSequence(true);
+            setAnnouncerMessage(`${attacker.name} has cast a spell!`);
+            await wait(1000);
+
+            turn === 0
+              ? setPlayerAnimation('magic')
+              : setOpponentAnimation('magic');
+            await wait(1000);
+
+            turn === 0
+              ? setPlayerAnimation('static')
+              : setOpponentAnimation('static');
+            await wait(500);
+
+            turn === 0
+              ? setOpponentAnimation('damage')
+              : setPlayerAnimation('damage');
+            await wait(750);
+
+            turn === 0
+              ? setOpponentAnimation('static')
+              : setPlayerAnimation('static');
+            setAnnouncerMessage(`${receiver.name} is dazed & confused!`);
+
+            turn === 0
+              ? setOpponentHealth(h => (h - damage > 0 ? h - damage : 0))
+              : setPlayerHealth(h => (h - damage > 0 ? h - damage : 0));
+            await wait(2500);
+
+            setAnnouncerMessage(`Now it's the turn of ${receiver.name}!`);
+            await wait(1500);
+
+            setTurn(turn === 0 ? 1 : 0);
+            setInSequence(false);
+          })();
+
           break;
+        }
+
+        case 'heal': {
+          const recovered = heal({ receiver: attacker });
+
+          (async () => {
+            setInSequence(true);
+            setAnnouncerMessage(`${attacker.name} has chosen to heal!`);
+            await wait(1000);
+
+            turn === 0
+              ? setPlayerAnimation('magic')
+              : setOpponentAnimation('magic');
+            await wait(1000);
+
+            turn === 0
+              ? setPlayerAnimation('static')
+              : setOpponentAnimation('static');
+            await wait(500);
+
+            setAnnouncerMessage(`${attacker.name} has recovered health.`);
+            turn === 0
+              ? setPlayerHealth(h =>
+                  h + recovered <= attacker.maxHealth
+                    ? h + recovered
+                    : attacker.maxHealth
+                )
+              : setOpponentHealth(h =>
+                  h + recovered <= attacker.maxHealth
+                    ? h + recovered
+                    : attacker.maxHealth
+                );
+            await wait(2500);
+
+            setAnnouncerMessage(`Now it's the turn of ${receiver.name}!`);
+            await wait(1500);
+
+            setTurn(turn === 0 ? 1 : 0);
+            setInSequence(false);
+          })();
+
+          break;
+        }
+
+        default: {
+          break;
+        }
       }
     }
   }, [sequence]);
